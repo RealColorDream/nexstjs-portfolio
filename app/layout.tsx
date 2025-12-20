1719e16835a6f41ca13dc206673a4724ba10bc23
RealColorDream <notrealcolordream@gmail.com>
2025-12-20 20:53:57 +0100

    Increase Navbar width

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { JetBrains_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const jetBrainsMono = JetBrains_Mono({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-jetbrains-mono",
+    display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${jetBrainsMono.variable} bg-white dark:bg-black font-sans transition-colors duration-300`}
       >
         {children}
       </body>
