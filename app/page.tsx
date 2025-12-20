49167197d69770a1d1c82bdf6e7111be07f3ed5a
RealColorDream <notrealcolordream@gmail.com>
2025-12-20 22:04:08 +0100

    update import order to be prettier

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
 import dynamic from 'next/dynamic'
 
 const Navbar = dynamic(() => import('./components/Navbar'))
+const HomePage = dynamic(() => import('./components/HomePage'))
 const Work = dynamic(() => import('./components/Work'))
 const Skills = dynamic(() => import('./components/Skills'))
 const Projects = dynamic(() => import('./components/Projects'))
-const HomePage = dynamic(() => import('./components/HomePage'))
 const Footer = dynamic(() => import('./components/Footer'))
 
 
@@ -13,7 +13,6 @@ export default function Home() {
     return (
         <div className="min-h-screen bg-white dark:bg-black">
             <Navbar/>
-
             <HomePage/>
             <Work/>
             <Skills/>
